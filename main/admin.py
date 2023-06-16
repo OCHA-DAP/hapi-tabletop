9172ccea1cdffb623ccaf475a177f0cd06761649
turnerm <turnermon@gmail.com>
2023-06-16 10:31:48 +0200

    Register models

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Indicator
+from .models import Country, Indicator, MetaData
 
 # Register your models here.
 
+admin.site.register(Country)
 admin.site.register(Indicator)
+admin.site.register(MetaData)
